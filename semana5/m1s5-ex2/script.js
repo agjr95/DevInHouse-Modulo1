3a1eb53e00ed2a899d8aeddf040c8b4bbd9eaa5a
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-04-07 12:18:52 -0300

    Adicionando style.css

--- semana5/m1s5-ex2/script.js
+++ semana5/m1s5-ex2/script.js
@@ -3,26 +3,18 @@ btn.addEventListener('click', () => {
   const inputsArray = document.querySelectorAll('input');
   const inputs = Array.from(inputsArray);
 
-  let span;
-  let grupo1 = inputs.filter(inputsElement => {
-    const inputFloatValue = parseFloat(inputsElement.value);
-    if (inputFloatValue >= 0 && inputFloatValue <= 1) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+  let grupo1 = inputs.filter(inputsElement =>
+    parseFloat(inputsElement.value) > 0 && parseFloat(inputsElement.value) <= 1
+      ? true
+      : false
+  );
   grupo1 = grupo1.map(inputsElement => {
-    console.log(grupo1);
-    const inputFloatValue = parseFloat(inputsElement.value);
-    document.querySelectorAll('span').forEach(item => {
-      item.remove();
-    });
-    span = document.createElement('span');
-    const color = '#' + Math.floor(inputFloatValue * 16777215).toString(16);
+    const span = document.createElement('span');
+    const color = '#' + Math.floor(inputsElement.value * 16777215).toString(16);
     span.innerText = color;
     span.style.backgroundColor = color;
-    return [span, inputFloatValue];
+    document.querySelectorAll('span').forEach(item => item.remove());
+    return [span, inputsElement];
     //o retorno será uma array dentro de outra, ou seja grupo1 = [[span,floatValue]]
   });
   grupo1.forEach(e => {
@@ -30,15 +22,18 @@ btn.addEventListener('click', () => {
     const span = e[0];
     const input = e[1];
     input.insertAdjacentElement('afterend', span);
+    input.style.border = '1px solid grey';
   });
 
-  const grupo2 = inputs.filter(inputsElement => {
-    const inputFloatValue = parseFloat(inputsElement.value);
-    if (!(inputFloatValue >= 0 && inputFloatValue <= 1)) {
-      return inputsElement;
-    }
-  });
+  const grupo2 = inputs.filter(inputsElement =>
+    !(
+      parseFloat(inputsElement.value) > 0 &&
+      parseFloat(inputsElement.value) <= 1
+    )
+      ? inputsElement
+      : null
+  );
   grupo2.forEach(e => {
-    e.style.border = '1px solid red';
+    e.style.border = '3px solid red';
   });
 });
