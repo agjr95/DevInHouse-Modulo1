cb8d1f0a4e0b21bedeff4fb848cc79bc0dd5f81f
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-06-21 14:05:58 -0300

    add: angular material

--- semana11/src/app/app.module.ts
+++ semana11/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StarWarsComponent } from './star-wars/star-wars.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, StarWarsComponent],
@@ -15,6 +17,8 @@ import { StarWarsComponent } from './star-wars/star-wars.component';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
